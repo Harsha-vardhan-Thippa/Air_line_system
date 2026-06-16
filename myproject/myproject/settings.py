1e0ae4f0afa3ec35e40c8f28c0163ce0ed296d98
Harsha Vardhan <harshavardhanthippa@gmail.com>
2026-06-16 14:03:44 +0530

    test console email

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -127,9 +127,10 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_TIMEOUT = 30
